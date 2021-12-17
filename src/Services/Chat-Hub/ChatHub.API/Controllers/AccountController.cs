64297bf3bd752c601cf1ea792b0f4d366e99f8a0
Leo <leocanbeverything@outlook.com>
2021-12-17 15:59:37 +0800

    create first project

---
+++ src/Services/Chat-Hub/ChatHub.API/Controllers/AccountController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatHub.API.Controllers
+{
+   
+    public class AccountController : BaseApiController
+    {
+    }
+}

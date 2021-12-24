895911b7c5c8f3a337def76557240ec1f13beb77
Leo <leocanbeverything@outlook.com>
2021-12-24 15:51:00 +0800

    AppUser Configuration

--- src/Services/Chat-Hub/Infrastructure/EntityConfigurations/AppUserConfiguration.cs
+++ src/Services/Chat-Hub/Infrastructure/EntityConfigurations/AppUserConfiguration.cs
@@ -9,10 +9,21 @@ namespace Infrastructure.EntityConfigurations
         public void Configure(EntityTypeBuilder<AppUser> builder)
         {
             // AppUser
-            builder.HasMany(ur => ur.UserRoles).WithOne(u => u.AppUser)
+            builder.ToTable(nameof(AppUser));
+
+            builder.HasMany(u => u.UserRoles).WithOne(u => u.AppUser)
                 .HasForeignKey(ur => ur.UserId).IsRequired();
 
-            builder.HasIndex(u => u.UserName).IsUnique(true);
+            builder.HasIndex(u => u.UserName).IsUnique().HasDatabaseName("IX_UserName");
+            builder.Property(u => u.KnownAs).HasMaxLength(256);
+            builder.Property(u => u.UserName).HasMaxLength(16);
+            builder.Property(u => u.Gender).HasMaxLength(8);
+            builder.Property(u => u.Introduction).HasMaxLength(512);
+            builder.Property(u => u.LookingFor).HasMaxLength(256);
+            builder.Property(u => u.Interests).HasMaxLength(256);
+            builder.Property(u => u.City).HasMaxLength(32);
+            builder.Property(u => u.Country).HasMaxLength(32);
+            
         }
     }
 }

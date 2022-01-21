47c0345bb03b0a7e3d24cc80a13b544f34fbebd9
Leo <leocanbeverything@outlook.com>
2022-01-21 13:30:15 +0800

    Register Unit test

--- src/Services/Chat-Hub/Domain/Common/OperationResult.cs
+++ src/Services/Chat-Hub/Domain/Common/OperationResult.cs
@@ -1,68 +1,35 @@
 ﻿namespace Domain.Common
 {
     public class OperationResult : OperationResult<object>
-    {
-        public OperationResult()
-        {
-        }
 
-        public OperationResult(ResultStatus status, string msg)
+
+    {
+      
+
+        public OperationResult(ResultStatus status, string msg = null): base(status, msg)
         {
             Status = status;
             Msg = msg;
         }
 
-        public OperationResult(ResultStatus status, string msg, object data)
+        public OperationResult(ResultStatus status,  object data, string msg = null) : base(status, msg)
         {
             Status = status;
             Msg = msg;
             Data = data;
         }
 
-        #region Method
-
-        public new static OperationResult Success()
-        {
-            return new(ResultStatus.Success, string.Empty);
-        }
-
-        public new static OperationResult Success(string msg)
-        {
-            return new(ResultStatus.Success, msg);
-        }
-
-        public static OperationResult Success(object data)
-        {
-            return new(ResultStatus.Success, string.Empty, data);
-        }
-
-
-        public new static OperationResult Error()
-        {
-            return new(ResultStatus.Error, string.Empty);
-        }
-
-        public new static OperationResult Error(string msg)
-        {
-            return new(ResultStatus.Error, msg);
-        }
-
-        #endregion
     }
 
     public class OperationResult<T>
     {
-        protected OperationResult()
-        {
-        }
-
-        private OperationResult(ResultStatus status, string msg)
+        protected OperationResult(ResultStatus status, string msg = null)
         {
             Status = status;
             Msg = msg;
         }
 
-        private OperationResult(ResultStatus status, T data, string msg)
+        protected OperationResult(ResultStatus status, T data, string msg = null)
         {
             Status = status;
             Msg = msg;
@@ -70,26 +37,13 @@
         }
 
         protected ResultStatus Status { get; set; }
-
-      
         public string Msg { get; set; }
-
-
         public T Data { get; set; }
-
-      
         public bool Ok => Status == ResultStatus.Success;
 
         #region Method
 
-      
-        public static OperationResult<T> Success()
-        {
-            return new(ResultStatus.Success, string.Empty);
-        }
-
-     
-        public static OperationResult<T> Success(string msg)
+        public static OperationResult<T> Success(string msg = null)
         {
             return new(ResultStatus.Success, msg);
         }
@@ -99,15 +53,7 @@
         {
             return new(ResultStatus.Success, data, msg );
         }
-
-       
-        public static OperationResult<T> Error()
-        {
-            return new(ResultStatus.Error, string.Empty);
-        }
-
-       
-        public static OperationResult<T> Error(string msg)
+        public static OperationResult<T> Error(string msg = null)
         {
             return new(ResultStatus.Error, msg);
         }
---
+++ src/Services/Chat-Hub/Tests/UnitTests/ControllerTests/AccountTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Application.Features.Account.Commands.CreateNewUser;
+using ChatHub.API.Controllers;
+using Domain.Common;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTests.ControllerTests
+{
+    public class AccountTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly Mock<IMediator> _mediatorMock;
+        public AccountTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+            _mediatorMock = new Mock<IMediator>();
+        }
+
+        [Fact]
+        public async Task Register_with_valid_model_should_pass()
+        {
+            //Arrange
+            var command = new CreateNewUserCommand();
+
+            _mediatorMock.Setup(m => m.Send(command, default))
+                .Returns(Task.FromResult(OperationResult<RegisteredUserDto>.Success()));
+            //Act
+            var controller = new AccountController(_mediatorMock.Object);
+            var result = await controller.Register(command);
+
+            
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+
+        }
+    }
+}

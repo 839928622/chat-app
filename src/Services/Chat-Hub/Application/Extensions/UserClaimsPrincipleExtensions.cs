bccae4632f05daff41a44f8ab6386ced298427be
Parker <jwk839928622@outlook.com>
2022-05-05 22:42:13 +0800

    modify user claims principle to suite vs2022

---
+++ src/Services/Chat-Hub/Application/Extensions/UserClaimsPrincipleExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+
+namespace Application.Extensions
+{
+    public static class UserClaimsPrincipleExtensions
+    {
+        /// <summary>
+        /// return ClaimTypes.Name value, throws an exception if value doesn't exist
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public static string GetRequiredUsername(this ClaimsPrincipal user)
+        {
+            // NameIdentifier = JwtRegisteredClaimNames.NameId
+            // Name = JwtRegisteredClaimNames.UniqueName 
+            return user.FindFirst(ClaimTypes.Name)?.Value!;
+        }
+
+        public static int GetRequiredUserId(this ClaimsPrincipal user)
+        {
+            // NameIdentifier = JwtRegisteredClaimNames.NameId
+            // Name = JwtRegisteredClaimNames.UniqueName 
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value!);
+        }
+
+       
+    }
+}

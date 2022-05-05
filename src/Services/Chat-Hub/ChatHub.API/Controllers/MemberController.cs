ae908e759b94c09f5853b0e1cf01d7305beeb47a
Parker <jwk839928622@outlook.com>
2022-05-05 22:43:12 +0800

    memberController

---
+++ src/Services/Chat-Hub/ChatHub.API/Controllers/MemberController.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using Application.Features.Account.Commands.CreateNewUser;
+using Application.Features.Photo.Queries.QueryUserPhotos;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatHub.API.Controllers
+{
+   // [Authorize]
+    public class MemberController : BaseApiController
+    {
+        private readonly IMediator _mediator;
+
+        public MemberController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("UserPhotos")]
+        [ProducesResponseType(typeof(IEnumerable<PhotoDto>), (int)HttpStatusCode.OK)]
+       
+        public async Task<ActionResult> UserPhotos([FromQuery]QueryUserPhotoRequest input)
+        {
+            var res = await _mediator.Send(input);
+                return Ok(res);
+        }
+
+    }
+}

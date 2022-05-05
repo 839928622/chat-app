bcfba076913a09d0fe5522ea1868757e70cdf8ce
Parker <jwk839928622@outlook.com>
2022-05-05 22:41:21 +0800

    signalr crerate message dto

---
+++ src/Services/Chat-Hub/Application/DataTransferObjects/SignalR/Message/CreateMessageDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DataTransferObjects.SignalR.Message
+{
+    public class CreateMessageDto
+    {
+        [Required]
+        public string RecipientUsername { get; set; } = null!;
+        [Required]
+        public string Content { get; set; } = null!;
+    }
+}

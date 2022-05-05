3f5575ab4e0735ff8e6fa58f563236ff9989deaa
Parker <jwk839928622@outlook.com>
2022-05-05 22:43:38 +0800

    appsettings

--- src/Services/Chat-Hub/ChatHub.API/Program.cs
+++ src/Services/Chat-Hub/ChatHub.API/Program.cs
@@ -1,5 +1,12 @@
 using Application.Extensions;
+using ChatHub.API.Middleware;
+using ChatHub.API.SignalR;
+using Domain.Entities;
 using Infrastructure.Extensions;
+using Infrastructure.Persistence;
+using Infrastructure.Repositories.Seed;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,46 +16,41 @@ builder.Services.AddEndpointsApiExplorer();
 var services=builder.Services.AddSwaggerGen();
 var configuration = builder.Configuration;
 services.AddControllers();
+services.AddSignalR();
 
 services.AddApplicationServices();
 services.AddInfrastructureServices(configuration);
 
 var app = builder.Build();
 
-
+//seeding
+await using var scope = app.Services.CreateAsyncScope();
+var servicesScope = scope.ServiceProvider;
+try
+{
+    var context = servicesScope.GetRequiredService<ChatAppContext>();
+    var userManager = servicesScope.GetRequiredService<UserManager<AppUser>>();
+    var roleManager = servicesScope.GetRequiredService<RoleManager<AppRole>>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(userManager, roleManager,context);
+}
+catch (Exception e)
+{
+    var logger = servicesScope.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "An error occurred during migration");
+}
 
 
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-//var summaries = new[]
-//{
-//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//};
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//       new WeatherForecast
-//       (
-//           DateTime.Now.AddDays(index),
-//           Random.Shared.Next(-20, 55),
-//           summaries[Random.Shared.Next(summaries.Length)]
-//       ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast");
-
 app.MapControllers();
+app.MapHub<PresenceHub>("hubs/presence");
+app.MapHub<MessageHub>("hubs/message");
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

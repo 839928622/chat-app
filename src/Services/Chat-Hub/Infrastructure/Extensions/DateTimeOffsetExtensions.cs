908d5aef9e1826493da6fe9bb8db81db302e4284
Parker <jwk839928622@outlook.com>
2022-05-06 22:08:00 +0800

    caculate age base on birthday(datetimeoffset)

---
+++ src/Services/Chat-Hub/Infrastructure/Extensions/DateTimeOffsetExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Infrastructure.Extensions
+{
+    public static class DateTimeOffsetExtensions
+    {
+        /// <summary>
+        /// calculate Age base on birthday(DateTimeOffset)
+        /// </summary>
+        /// <param name="dateTimeOffset"></param>
+        /// <returns></returns>
+        public static int CalculateAge(this DateTimeOffset dateTimeOffset)
+        {
+            var today = DateTimeOffset.Now;
+            var age = today.Year - dateTimeOffset.Year;
+            if (today.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}

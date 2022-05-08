afbdfdc5f9f2a67de7c99b2a942193a15cde6360
Parker <jwk839928622@outlook.com>
2022-05-08 13:29:45 +0800

    when swap language, ngx-timeago should change as well

--- src/Web/WebSPA/client/src/app/translate/translate.component.ts
+++ src/Web/WebSPA/client/src/app/translate/translate.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-
+import { TimeagoIntl } from 'ngx-timeago';
+import {strings as englishStrings} from 'ngx-timeago/language-strings/en';
+import {strings as ChineseStrings} from 'ngx-timeago/language-strings/zh-CN';
 @Component({
   selector: 'app-translate',
   templateUrl: './translate.component.html',
@@ -11,11 +13,11 @@ export class TranslateComponent implements OnInit {
     'en-US': 'English',
     'zh-CN': '中文简体'
   };
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private intl: TimeagoIntl) {
     translate.addLangs(['en-US', 'zh-CN']);
     translate.setDefaultLang('zh-CN');
     const browerLanguage = translate.getBrowserLang();
-    console.log(browerLanguage);
+    // console.log(browerLanguage);
     switch (browerLanguage) {
      case 'en':
       translate.use('en-US');
@@ -33,7 +35,20 @@ export class TranslateComponent implements OnInit {
   }
 
   useLanguage(language: string): void {
-
-    this.translate.use(language);
+    switch (language) {
+      case 'en-US':
+        this.translate.use('en-US');
+        this.intl.strings = englishStrings;
+        break;
+      case 'zh-CN':
+          this.translate.use('zh-CN');
+          this.intl.strings = ChineseStrings;
+          break;
+      default:
+        this.translate.use('en-US');
+        this.intl.strings = englishStrings;
+        break;
+    }
+    this.intl.changes.next();
   }
 }
